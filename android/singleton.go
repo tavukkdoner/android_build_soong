f5531bc060c8f0034e0c09099b6860051ef2a554
Colin Cross <ccross@android.com>
2023-12-14 14:46:23 -0800

    Convert ModuleProvder to generic providers API
    Convert all of the callers of ModuleProvider/ModuleHasProvider to use the
    type-safe android.SingletonModuleProvider API.
    
    Bug: 316410648
    Test: builds
    Change-Id: I6f11638546b64749e451cebbf33140248dc1d193

--- android/singleton.go
+++ android/singleton.go
@@ -33,15 +33,7 @@ type SingletonContext interface {
 	// Allows generating build actions for `referer` based on the metadata for `name` deferred until the singleton context.
 	ModuleVariantsFromName(referer Module, name string) []Module
 
-	// ModuleProvider returns the value, if any, for the provider for a module.  If the value for the
-	// provider was not set it returns the zero value of the type of the provider, which means the
-	// return value can always be type-asserted to the type of the provider.  The return value should
-	// always be considered read-only.  It panics if called before the appropriate mutator or
-	// GenerateBuildActions pass for the provider on the module.
-	ModuleProvider(module blueprint.Module, provider blueprint.ProviderKey) interface{}
-
-	// ModuleHasProvider returns true if the provider for the given module has been set.
-	ModuleHasProvider(module blueprint.Module, provider blueprint.ProviderKey) bool
+	moduleProvider(module blueprint.Module, provider blueprint.AnyProviderKey) (any, bool)
 
 	ModuleErrorf(module blueprint.Module, format string, args ...interface{})
 	Errorf(format string, args ...interface{})
@@ -282,3 +274,8 @@ func (s *singletonContextAdaptor) ModuleVariantsFromName(referer Module, name st
 	}
 	return result
 }
+
+func (s *singletonContextAdaptor) moduleProvider(module blueprint.Module, provider blueprint.AnyProviderKey) (any, bool) {
+	return s.SingletonContext.ModuleProvider(module, provider)
+}
+
