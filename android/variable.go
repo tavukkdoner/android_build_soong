7e7f699913e371d979d5d850f2aa5117c9895c23
minaripenguin <minaripenguin@users.noreply.github.com>
2024-05-02 08:29:03 +0800

    Revert "build: soong: use jemalloc by default and allow opt-in to scudo (2/4)"
    This reverts commit 6d8e94d88268e43d2eddea382d9d5d6ae4687e57.

--- android/variable.go
+++ android/variable.go
@@ -69,20 +69,12 @@ type variableProperties struct {
 			Enabled *bool `android:"arch_variant"`
 		} `android:"arch_variant"`
 
-		Malloc_use_scudo struct {
-			Cflags              []string `android:"arch_variant"`
-			Shared_libs         []string `android:"arch_variant"`
-			Whole_static_libs   []string `android:"arch_variant"`
-			Exclude_static_libs []string `android:"arch_variant"`
-			Srcs                []string `android:"arch_variant"`
-			Header_libs         []string `android:"arch_variant"`
-		} `android:"arch_variant"`
-
 		Malloc_not_svelte struct {
 			Cflags              []string `android:"arch_variant"`
 			Shared_libs         []string `android:"arch_variant"`
 			Whole_static_libs   []string `android:"arch_variant"`
 			Exclude_static_libs []string `android:"arch_variant"`
+			Srcs                []string `android:"arch_variant"`
 			Header_libs         []string `android:"arch_variant"`
 		} `android:"arch_variant"`
 
@@ -291,7 +283,6 @@ type ProductVariables struct {
 	Unbundled_build_image        *bool    `json:",omitempty"`
 	Always_use_prebuilt_sdks     *bool    `json:",omitempty"`
 	Skip_boot_jars_check         *bool    `json:",omitempty"`
-	Malloc_use_scudo             *bool    `json:",omitempty"`
 	Malloc_not_svelte            *bool    `json:",omitempty"`
 	Malloc_not_svelte_libc32     *bool    `json:",omitempty"`
 	Malloc_zero_contents         *bool    `json:",omitempty"`
@@ -326,6 +317,8 @@ type ProductVariables struct {
 	EnableCFI       *bool    `json:",omitempty"`
 	CFIExcludePaths []string `json:",omitempty"`
 	CFIIncludePaths []string `json:",omitempty"`
+
+	DisableScudo *bool `json:",omitempty"`
 
 	MemtagHeapExcludePaths      []string `json:",omitempty"`
 	MemtagHeapAsyncIncludePaths []string `json:",omitempty"`
@@ -619,7 +612,6 @@ func (v *ProductVariables) SetDefaultConfig() {
 		AAPTCharacteristics: stringPtr("nosdcard"),
 		AAPTPrebuiltDPI:     []string{"xhdpi", "xxhdpi"},
 
-		Malloc_use_scudo:             boolPtr(false),
 		Malloc_not_svelte:            boolPtr(true),
 		Malloc_not_svelte_libc32:     boolPtr(true),
 		Malloc_zero_contents:         boolPtr(true),
